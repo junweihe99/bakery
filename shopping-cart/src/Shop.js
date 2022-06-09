02159b082edf63614abdfaf8054f0e6d43fc7148
Junwei <junwei.he.99@gmail.com>
2022-06-08 19:16:59 -0700

    Edited display setting when resizing window

--- shopping-cart/src/Shop.js
+++ shopping-cart/src/Shop.js
@@ -3,11 +3,14 @@ import Cart from "./components/Cart";
 
 export default function Shop() {
     return (
-        <div style={{display: "flex", justifyContent: "space-between"}}>
+        <div style={{display: "flex", justifyContent: "space-between", flexWrap:"wrap"}}>
             <div className="ms-4" style={{display: "flex", flexWrap: "wrap", gap:"2rem"}}>
                 <Product />
+                <Product />
+                <Product />
+                <Product />
             </div>
-            <div>
+            <div className="ms-4">
                 <Cart />
             </div>
         </div>
